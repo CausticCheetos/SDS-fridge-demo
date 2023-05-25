35b3f4d1df27e3bfad95b4dc2205395fce90b656
WeissSchneeee <yuanming.huang@student.uts.edu.au>
2023-05-25 10:29:35 +1000

    latest flow API

--- Backend/SDSFridge/base/urls.py
+++ Backend/SDSFridge/base/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('rtp/<int:greater>/<int:lesser>',views.get_past_rtp, name = 'get_past_rtp'),
     path('rtp/',views.get_rtp, name = 'get_rtp'),
     path('maxigauge/latest/',views.get_maxigauge_latest, name = 'get_maxigauge_latest'),
+    path('flow/latest/',views.get_flow_latest, name = 'get_flow_latest'),
     path('valves/latest/',views.get_valves, name = 'get_valves'),
     path('parameters/',views.post_parameters, name = 'post_parameters'),
     path('getParameters/',views.get_parameters, name = 'get_parameters'),

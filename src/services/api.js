04c7673dfb390a797887d00e1ed28a560f8c82a2
WeissSchneeee <yuanming.huang@student.uts.edu.au>
2023-05-30 19:20:14 +1000

    forgot to cd

--- src/services/api.js
+++ src/services/api.js
@@ -20,11 +20,52 @@ const getFlow = () => {
     .then((response) => response.json())
 } 
 
+const createEmail = async(email) => {
+    await fetch(`${baseUrl}/create_Email/`,{
+        method: 'POST',
+        headers: {
+            // 'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(email)
+        })
+}
+
+const getEmail = (id) => {
+    return fetch(`${baseUrl}/emailDetail/${id}`)
+    .then((response) => response.json())
+} 
+
+const getEmails = () => {
+    return fetch(`${baseUrl}/getEmails`)
+    .then((response) => response.json())
+} 
+
+const updateEmail = async(email) => {
+    await fetch(`${baseUrl}/update_Email/`,{
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(email)
+        })
+    }
+
+const deleteEmail = (id) => {
+    fetch(`${baseUrl}/delete_Email/${id}`,{method: "DELETE"})
+}
+
 const exported = {
     getMaxi,
     getRTP,
     getValves,
-    getFlow
+    getFlow,
+    createEmail,
+    getEmail,
+    getEmails,
+    updateEmail,
+    deleteEmail
 }
 
 export default exported
